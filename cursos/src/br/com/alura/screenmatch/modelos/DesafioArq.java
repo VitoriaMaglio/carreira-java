0226e0232d643e84da44c432a3969840c0cad437
VitoriaMaglio <vitoriamaglio@gmail.com>
2026-02-05 12:12:41 -0300

    Exercícios do curso: lendo e escrevendo em arq para revisão com java.io

---
+++ cursos/src/br/com/alura/screenmatch/modelos/DesafioArq.java
@@ -0,0 +1,38 @@
+package br.com.alura.screenmatch.modelos;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class DesafioArq {
+
+    public static void main(String[] args) {
+        //CRIAR ARQ E ESCREVER ALGO
+        //Declara o q vc quer escrever, cria o arq com try. chama o método de escrita
+        String data = "Conteúdo a ser gravado no arquivo.";
+        try (FileWriter writer = new FileWriter("arquivo.txt")) {
+            writer.write(data);
+            System.out.println("Dados gravados com sucesso.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Escrever no arquivo o q o user escreve no terminal
+        Scanner escreva = new Scanner(System.in);
+        System.out.println("Escreva o q vc quer registar no arq: ");
+        String dados = escreva.nextLine();
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("saida.txt"))){
+            writer.write(dados);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        //Lê o q o user escreveu
+
+        try(BufferedReader reader = new BufferedReader(new FileReader("saida.txt"))){
+            String linha = reader.readLine();
+            System.out.println(linha);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
